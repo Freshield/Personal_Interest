23d19f8274122c7c027cb140675aa19f2bbf043a
freshield <zxdsw199182@gmail.com>
2018-02-06 21:13:06 +0800

    interrupt

---
+++ 03_DICOM_Processing/01_PreProcess_Tutorial/Preprocess_tutorial.py
@@ -0,0 +1,45 @@
+import numpy as np
+import pandas as pd
+import dicom
+import os
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+from skimage import measure, morphology
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+INPUT_FOLDER = 'input/sample_images/'
+patients = os.listdir(INPUT_FOLDER)
+patients.sort()
+
+#################################################
+########   LOAD THE FILES   #####################
+#################################################
+
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+def get_pixels_hu(slices):
+    image = np.stack([s.pixel_array for s in slices])
+    # Convert to int16 (from sometimes int16),
+    # should be possible as values should always be low enough (<32k)
+    image = image.astype(np.int16)
+
+    # Convert to Hounsfield units (HU)
+    for slice_number in range(len(slices)):
+
+        intercept = slices[slice_number].RescaleIntercept
+        slope = slices[slice_number].RescaleSlope
+
+        
