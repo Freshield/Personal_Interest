dbf0523954f8e6523f2e8dc6575b663f90bfcaad
Freshield <zxdsw199182@gmail.com>
2019-11-05 18:07:54 +0800

    done the cut model try

---
+++ a14_try_hdf5/b7_check_model.py
@@ -0,0 +1,66 @@
+#coding=utf-8
+"""
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: b7_check_model.py
+@Time: 2019-11-05 17:40
+@Last_update: 2019-11-05 17:40
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+"""
+import os
+import h5py
+import numpy as np
+
+os.remove('data/small_model.h5')
+
+f = h5py.File('data/model.h5', 'r')
+print(dict(f.attrs))
+
+f1 = h5py.File('data/small_model.h5', 'w')
+
+for key, attr in f.attrs.items():
+    f1.attrs[key] = attr
+
+for key, value in f.items():
+    print(key)
+    print(value)
+    print(value.name)
+    print()
+
+
+def getitems(input_item):
+    rst_list = []
+    if type(input_item) == h5py._hl.files.File:
+        for key, value in input_item.items():
+            rst_list += getitems(value)
+    elif type(input_item) == h5py.Group:
+        if len(input_item) == 0:
+            rst_list.append((input_item.name, None))
+        else:
+            for key, value in input_item.items():
+                rst_list += getitems(value)
+    elif type(input_item) == h5py.Dataset:
+        a = input_item[:]
+        rst_list.append((input_item.name, a.copy()))
+
+    return rst_list
+
+
+
+rst_list = getitems(f)
+
+for i in range(100):
+    if rst_list[i][1] is None:
+        f1.create_group(rst_list[i][0])
+    else:
+        f1[rst_list[i][0]] = rst_list[i][1]
+
+f.close()
+f1.close()
