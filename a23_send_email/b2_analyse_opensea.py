b375723d7a1a2cec0bcf66d7f40915ccc2d405a8
freshield <yangyufresh@163.com>
2022-03-01 17:22:16 +0800

    update run files

--- a23_send_email/b2_analyse_opensea.py
+++ a23_send_email/b2_analyse_opensea.py
@@ -14,6 +14,7 @@
 @                                    Freshield @
 @==============================================@
 """
+import os
 import redis
 import logging
 import traceback
@@ -69,3 +70,23 @@ if __name__ == '__main__':
             logging.info(traceback.format_exc())
         finally:
             browser.close()
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--headless')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--remote-debugging-port=9222")
+
+try:
+  driver = webdriver.Chrome(chrome_options=options)
+  driver.get("https://www.google.com")
+  s = driver.find_element_by_name("q")
+  assert s.is_displayed() is True
+  print("ok")
+except Exception as ex:
+  print(ex)
+
+driver.quit()
