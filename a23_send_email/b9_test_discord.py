f1451595e348af58cc507e668b67f1ba469f021b
freshield <yangyufresh@163.com>
2022-03-03 15:18:07 +0800

    add channel ids

--- a23_send_email/b9_test_discord.py
+++ a23_send_email/b9_test_discord.py
@@ -17,7 +17,7 @@
 import redis
 import discord
 from discord.ext import tasks
-from config import discord_token, channel_id
+from config import discord_token, channel_id_dict
 
 
 class MyClient(discord.Client):
@@ -31,21 +31,24 @@ class MyClient(discord.Client):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
+        channel = client.get_channel(channel_id_dict['二次元社区'])
+        await channel.send('begin')
         print('------')
 
     @tasks.loop(seconds=2)
     async def my_background_task(self):
-        channel = client.get_channel(channel_id)
         with redis.Redis(connection_pool=self.pool) as r:
             for i in range(r.llen('new_info')):
                 info = r.lpop('new_info')
                 print(info)
-                await channel.send(info)
+                for key, channel_id in channel_id_dict.items():
+                    channel = client.get_channel(channel_id)
+                    await channel.send(info)
 
     @my_background_task.before_loop
     async def before_my_task(self):
         await self.wait_until_ready()# wait until the bot logs in
 
-
+print('begin server')
 client = MyClient()
 client.run(discord_token)
